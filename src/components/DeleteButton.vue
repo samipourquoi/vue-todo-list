3e4cbfc49b9e893264dfe2be076581986df39ff3
samipourquoi <sami.guiderdoni@gmail.com>
2020-11-14 01:31:42 +0100

    Add 'delete all' button

---
+++ src/components/DeleteButton.vue
@@ -0,0 +1,40 @@
+<template>
+<div class="delete-button">
+	<button type="button" @click="deleteAll">delete all</button>
+</div>
+</template>
+
+<script>
+export default {
+	name: "DeleteButton",
+	methods: {
+		deleteAll() {
+			this.$store.state.list = this.$store.state.list.filter(e => !e.checked);
+		}
+	}
+}
+</script>
+
+<style scoped>
+.delete-button {
+	margin: 40px 0 40px 40px;
+	float: right;
+}
+
+button {
+	color: white;
+	height: 40px;
+	width: 140px;
+	font-family: "Montserrat", sans-serif;
+	font-weight: 400;
+	font-size: 14px;
+	background-color: #EB5757;
+	border-radius: 10px;
+	border: none;
+}
+
+button:focus {
+	outline: none;
+	box-shadow: 0 2px 6px rgba(0, 0, 0, 0.20);
+}
+</style>
--- src/views/Completed.vue
+++ src/views/Completed.vue
@@ -1,17 +1,27 @@
 <template>
-	<div class="completed">
+	<div class="completed" v-if="$store.state.list.some(e => e.checked)">
 		<Todo :list="$store.state.list.filter(e => e.checked)" deletable/>
+		<DeleteButton/>
+	</div>
+	<div v-else>
+		<h2>Oops! It's all empty!</h2>
 	</div>
 </template>
 
 <script>
 import Todo from "@/components/Todo";
+import DeleteButton from "@/components/DeleteButton";
 export default {
 	name: "Completed",
-	components: { Todo }
+	components: { DeleteButton, Todo }
 }
 </script>
 
 <style scoped>
-
+h2 {
+	color: #333333;
+	text-align: center;
+	font-family: "Raleway", sans-serif;
+	font-weight: 700;
+}
 </style>
